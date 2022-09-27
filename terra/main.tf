bfef93508a050755cf5e2c3d87896c5deee586fd
slalam2 <114500927+slalam2@users.noreply.github.com>
2022-09-27 22:20:39 +0530

    Update main.tf

--- terra/main.tf
+++ terra/main.tf
@@ -13,3 +13,13 @@ resource "azurerm_resource_group" "rg" {
     }
 
 }
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "dialogstorage1"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = "eastus"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+
+}
