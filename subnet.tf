9ed29df92f60bd7ae767d6e8641bb32074b6bfd9
slalam2 <114500927+slalam2@users.noreply.github.com>
2022-09-27 20:33:57 +0530

    Delete subnet.tf

--- subnet.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_subnet" "subnet" {
-  name                 = "testsub"
-  resource_group_name  = azurerm_resource_group.RG.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-}
