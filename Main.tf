41d836bfe71d956ed348fc1d509f28b330e2b0f5
slalam2 <114500927+slalam2@users.noreply.github.com>
2022-09-27 20:30:33 +0530

    Create Main.tf

---
+++ Main.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {
+  features {}
+
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "demoRG"
+  location = var.location
+    tags = {
+      "Environment" = "test"
+      "Owner" = "Pawan"
+      
+    }
