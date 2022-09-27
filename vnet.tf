f70d90a970be8e77990bded2ef4830978c036dae
slalam2 <114500927+slalam2@users.noreply.github.com>
2022-09-27 20:32:57 +0530

    Delete vnet.tf

--- vnet.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = "demoVnet"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.RG.name
-  address_space       = ["10.0.0.0/16"]
-
-
-}
